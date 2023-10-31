c401a212185b6756c1f1f292abd6b45e8777be91
SediaM <sediamassaquoi@gmail.com>
2023-10-31 18:49:45 -0400

    register route added to src-sw.js file. Dist folder generated.

--- client/src-sw.js
+++ client/src-sw.js
@@ -1,3 +1,4 @@
+const { StaleWhileRevalidate } = require('workbox-strategies');
 const { offlineFallback, warmStrategyCache } = require('workbox-recipes');
 const { CacheFirst } = require('workbox-strategies');
 const { registerRoute } = require('workbox-routing');
@@ -27,4 +28,16 @@ warmStrategyCache({
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
 // TODO: Implement asset caching
-registerRoute();
+registerRoute(
+  ({ request }) => ['style', 'script', 'worker'].includes(request.destination),
+  new StaleWhileRevalidate({
+    // Name of the cache storage.
+    cacheName: 'asset-cache',
+    plugins: [
+      // This plugin will cache responses with these headers to a maximum-age of 30 days
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+    ],
+  })
+);
